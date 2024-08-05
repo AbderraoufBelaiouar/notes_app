ba74e95452a3ef4bc6c39ac4b172e70657f6f1ff
AbderraoufBelaiouar <abderraouf4567@gmail.com>
2024-08-05 12:14:12 +0100

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,53 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class CustomNoteItem extends StatelessWidget {
   const CustomNoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 16, bottom: 16, left: 16),
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 241, 196, 128),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          ListTile(
-            title: const Text(
-              "Flutter tips",
-              style: TextStyle(color: Colors.black, fontSize: 26),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const EditNoteView(),
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 16, bottom: 16, left: 16),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 241, 196, 128),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              title: const Text(
+                "Flutter tips",
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+                child: Text(
+                  "build your carrer with tharwat say",
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(.5), fontSize: 18),
+                ),
+              ),
+              trailing: IconButton(
+                icon: Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black.withOpacity(.4),
+                  size: 30,
+                ),
+                onPressed: () {},
+              ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
               child: Text(
-                "build your carrer with tharwat say",
+                "may,27,2024",
                 style: TextStyle(
-                    color: Colors.black.withOpacity(.5), fontSize: 18),
+                  color: Colors.black.withOpacity(.4),
+                ),
               ),
             ),
-            trailing: IconButton(
-              icon: Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black.withOpacity(.4),
-                size: 30,
-              ),
-              onPressed: () {},
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: Text(
-              "may,27,2024",
-              style: TextStyle(
-                color: Colors.black.withOpacity(.4),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/views/widgets/custom_view_body.dart
+++ lib/views/widgets/custom_view_body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/views/widgets/custom_note_item.dart';
 import 'package:notes_app/views/widgets/custom_view_appbar.dart';
 import 'package:notes_app/views/widgets/notes_list_view.dart';
 
