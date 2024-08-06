3847ac0fc25e6865cc63881f95479fd79e875b92
AbderraoufBelaiouar <abderraouf4567@gmail.com>
2024-08-06 23:31:40 +0100

    delete note

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -46,7 +46,9 @@ class CustomNoteItem extends StatelessWidget {
                   color: Colors.black.withOpacity(.4),
                   size: 30,
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  note.delete();
+                },
               ),
             ),
             Padding(
